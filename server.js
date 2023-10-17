0053a046e68e933cf68a32af908d5bb6850c6c31
Michael Hill <michael.hill1167@btinternet.com>
2023-10-17 21:09:35 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const findPrime = require('./utils/findPrime');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Successful response.');
+  const n = req.query.n;
+
+  if (!n) {
+    res.render('index');
+    return;
+  }
+  
+  const prime = findPrime(n);
+
+  const locals = { n, prime };
+  res.render('index', locals);
 });
 
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
