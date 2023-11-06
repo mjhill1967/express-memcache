9e689f0357c7a28a02269a7eae70cd6c784974b4
Michael Hill <michael.hill1167@btinternet.com>
2023-11-06 21:48:13 +0000

    Improved error handling

--- utils/getOrders.js
+++ utils/getOrders.js
@@ -8,16 +8,18 @@ const client = new Client({
 });
 
 exports.getOrders = async ( params ) => { 
-console.log( params );
+
 if (params.location == undefined ) {
   params.location = 'LJZAMNQFK7X0V';  
 }
 if (params.startAt == undefined ) {
-  params.startAt = '2023-11-05T00:00:00+00:00';  
+  params.startAt = '2023-11-05T00:00:00';  
 }
 if (params.endAt == undefined ) {
-  params.endAt = '2023-11-05T23:59:00+00:00';  
+  params.endAt = '2023-11-05T23:59:00';  
 }
+console.log("Attempt to search orders");
+console.log( params );
 try {
   const response = await client.ordersApi.searchOrders({
     locationIds: [
@@ -44,37 +46,46 @@ try {
     }
   });
   tickets = response.result.orders;
+  if (tickets == undefined) {
+    tickets = [];
+  } 
   const data = [];
   var ticketData = {};
   let x = 0;
+  console.log('Found ' + tickets.length + ' records');
   if ( tickets.length > -1 ) {
     var txtOutput = '';
     data[ x ] = 'ticket_type;qty;tax;gross;discount;base_price;variation_price'
     x = x + 1;
 	for (let i = 0; i < tickets.length; i++) {
-		// console.log( 'Line items: ' + tickets[i].lineItems );
+
 		ticketItems = tickets[i].lineItems;
 		if ( ticketItems != undefined ) {
-			// console.log( 'Items is ' + ticketItems.length );
+			
 			for (let j = 0; j < ticketItems.length; j++) {
 				
 				if ( ticketItems[j] != undefined ) { 
           let ticket_qty = ticketItems[j].quantity
           if ( ticketItems[j].name != undefined ) {
             let ticket_text = ticketItems[j].name;
-            let ticket_type = ticket_text.replace(" Match ticket", "");
-            ticket_type = ticket_type.replace("Conc", "Concession");
-            ticket_type = ticket_type.replace("U18", "Under 18");
-            ticket_type = ticket_type.replace("U12", "Under 12");
-            ticket_type = ticket_type.replace("FAMILY ", "FAMILY of ");
-            ticket_type = ticket_type.replace("NextGen 18-21", "Next Gen 18-21");
+            let ticket_type = ticket_text;
+            if ( params.location == 'LJZAMNQFK7X0V' ) {
+              ticket_type = ticket_text.replace(" Match ticket", "");
+              ticket_type = ticket_type.replace("Conc", "Concession");
+              ticket_type = ticket_type.replace("U18", "Youth (12-17)");
+              ticket_type = ticket_type.replace("U12", "Child (Under 12)");
+              ticket_type = ticket_type.replace("FAMILY ", "FAMILY of ");
+              ticket_type = ticket_type.replace("NextGen 18-21", "Next Gen (18-23)");
+              ticket_type = ticket_type.replace("NextGen 18-23", "Next Gen (18-23)");
+            } else {
+              console.log('Not a ticket');
+            }
             let ticket_tax = ticketItems[j].totalTaxMoney;
             let ticket_gross = ticketItems[j].totalMoney;
             let ticket_discount = ticketItems[j].totalDiscountMoney;
             let ticket_base_price = ticketItems[j].basePriceMoney;
             let ticket_variation_price = ticketItems[j].variationTotalPriceMoney;
 
-            // console.log( ticketItems[j]);
             if ( ticketData[ticket_type] == undefined ) {
               ticketData[ticket_type] = {};
               ticketData[ticket_type].qty = 0;
@@ -91,12 +102,13 @@ try {
               ticketData[ticket_type].variation_price = ticketData[ticket_type].variation_price + parseFloat(ticket_variation_price.amount);
               ticketData[ticket_type].tax = ticketData[ticket_type].tax + parseFloat(ticket_tax.amount);
             }
-            // console.log( ticket_type );
+            
             // txtOutput = txtOutput + '<div style="padding:2px;"><strong>' + ticket_type + '</strong> x ' + ticket_qty + '</div>' ;
             data[ x ] = ticket_type + ';' + ticket_qty + ';' + ticket_tax.amount + ';' + ticket_gross.amount + ';' + ticket_discount.amount + ';' + ticket_base_price.amount + ';' + ticket_variation_price.amount + ';';
             txtOutput = txtOutput + '<div style="padding:2px;"><strong>' + data[x] + '</div>' ;
             x = x + 1;
           } else {
+            console.log('Name is undefined');
             if ( ticketItems[j].itemType == 'CUSTOM_AMOUNT' ) {
               txtOutput = txtOutput + '*** Custom amount' + ' x ' + ticket_qty + '<br/>' ;
               data[ x ] = 'Custom amount' + ';' + ticket_qty + ';'
@@ -104,7 +116,7 @@ try {
             } else { console.log( ticketItems[j] ); }
           }
           
-				};
+				}
 				
 			}
 
@@ -113,11 +125,15 @@ try {
 	}
     console.log('Success');
     return ticketData;
+  } else { 
+    return JSON.parse('No data found');
   }
-} catch(error) {
-  console.log('Square error');
-  console.log(error);
-  return error;
+} catch( error)  {
+    const errBody = JSON.parse(error.body);
+    const errorDetails = errBody.errors[0];
+    errorDetails.statusCode = error.statusCode;
+    console.log("Error " + errorDetails.statusCode);
+    return errorDetails;
 }
 
 }
